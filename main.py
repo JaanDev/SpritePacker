814fe0ec9fa5418818b1b761cee31fd2b1b486c4
JaanDev <67502867+JaanDev@users.noreply.github.com>
2024-06-25 20:13:45 +0300

    support globbing

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import argparse
 from pathlib import Path
 from colorama import Fore, Style, init as colorama_init
+from glob import glob
+from typing import List
 
 import unpacker
 import packer
 
 
+def globbed_list(p: List[Path]) -> List[Path]:
+    ret = []
+    for el in p:
+        ret.extend(map(Path, glob(str(el))))
+    return list(dict.fromkeys(ret))  # remove duplicates
+
+
 def processCommand(args: argparse.Namespace) -> None:
     args.output.mkdir(exist_ok=True, parents=True)
-    args.input = list(set(args.input))  # remove duplicates
+
+    args.input = globbed_list(args.input)
 
     if args.action == "unpack":
         for val in args.input:
@@ -21,7 +31,7 @@ def processCommand(args: argparse.Namespace) -> None:
 
         unpacker.unpack(args.input, args.output)
     elif args.action == "pack":
-        args.original = list(set(args.original))  # remove duplicates
+        args.original = globbed_list(args.original)
 
         for val in args.input:
             if not val.exists() or not val.is_dir():
@@ -44,10 +54,10 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("action", choices=("pack", "unpack"), help="An action to use")
-    parser.add_argument("-i", "--input", nargs="+", help="Input plist files (for unpacking)/dirs (for packing)", type=Path)
+    parser.add_argument("-i", "--input", nargs="+", help="Input plist files (for unpacking)/dirs (for packing). Supports globbing", type=Path)
     parser.add_argument("-o", "--output", help="Specify an output directory", type=Path)
     parser.add_argument("-O", "--original", nargs="*",
-                        help="Optional. Original plist files for packing to provide sprite offsets so changed sprites don't look offset. All of them are used for every dir", type=Path, default=[])
+                        help="Optional. Original plist files for packing to provide sprite offsets so changed sprites don't look offset. All of them are used for every dir. Supports globbing", type=Path, default=[])
     parser.add_argument("-p", "--padding", help="Optional. Padding for packing (default is 2)", type=int, default=2)
 
     processCommand(parser.parse_args())
